8ce02ee7bb55fda8d3d685ca6ee73f78e569be09
William <stewart-w24@hotmail.co.uk>
2020-12-21 14:17:21 +0000

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -moz-osx-font-smoothing: greyscale;
+
+  color: white;
+
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  background: black;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="hello">
+    <div class="menu">
+        <div class="menu-item">
+          <div class="menu-text">
+            Resume
+          </div>
+        </div>
+      <div class="menu-item">
+        <div class="menu-text">
+          Projects
+        </div>
+        </div>
+      <div class="menu-item menu-center">
+        <div class="menu-text">
+          William Stewart
+        </div>
+      </div>
+        <div class="menu-item">
+          <div class="menu-text">
+          Snippets
+          </div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-text">
+          About
+          </div>
+        </div>
+    </div>
+
+    <div class="name">
+      <div class="letter">
+        <img src="../assets/img/W.png" class="letter-image" alt="W">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/I.png" class="letter-image" alt="I">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/L.png" class="letter-image" alt="L">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/L.png" class="letter-image" alt="L">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name {
+  border: 1px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  width: 100vw;
+  height: 100vh;
+}
+
+.letter {
+  margin: .8%;
+  filter: drop-shadow(0px 0px 7px rgba(150, 150, 150, .7));
+}
+
+.letter-image {
+  max-width: 100%;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+}
+
+.menu-item {
+  flex: 1;
+  text-align: center;
+}
+
+.menu-text {
+  font-size: 20px;
+  color: #818181;
+  padding: 10px;
+  border: 2px solid #080808;
+  cursor: pointer;
+  width: fit-content;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.menu-text:hover {
+  border-bottom: 2px solid #c278ff;
+}
+
+.menu-center {
+  font-size: 36px;
+  color: #eaeaea;
+}
+</style>
